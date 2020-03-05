09967e6c5ee126f0e5fe5e69004b11a6373068ae
Bradley Grainger <bgrainger@gmail.com>
2020-03-04 20:27:12 -0800

    Add RomuDuo.

---
+++ src/RomuRandom/RomuDuo.cs
@@ -0,0 +1,77 @@
+// Copyright 2020 Mark A. Overton
+// Copyright 2020 Bradley Grainger
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+#if !NETSTANDARD1_0
+using System.Numerics;
+#endif
+
+namespace RomuRandom
+{
+	/// <summary>
+	/// Implements the "RomuDuo" generator from <a href="http://romu-random.org/">romu-random.org</a>.
+	/// </summary>
+	public sealed class RomuDuo
+	{
+		/// <summary>
+		/// Initializes a new instance of <see cref="RomuDuo"/>, seeded with the current time.
+		/// </summary>
+		public RomuDuo()
+		{
+			_yState = unchecked((ulong) DateTime.UtcNow.Ticks);
+		}
+
+		/// <summary>
+		/// Initializes a new instance of <see cref="RomuDuo"/> with the specified seed.
+		/// </summary>
+		/// <param name="seed">The non-zero seed for the random number generator.</param>
+		public RomuDuo(int seed)
+		{
+			if (seed == 0)
+				throw new ArgumentOutOfRangeException(nameof(seed), "seed must not be zero");
+			_yState = unchecked((ulong) seed);
+		}
+
+		/// <summary>
+		/// Initializes a new instance of <see cref="RomuDuo"/> with the specified seeds.
+		/// </summary>
+		public RomuDuo(ulong seed1, ulong seed2)
+		{
+			if (seed1 == 0 && seed2 == 0)
+				throw new ArgumentOutOfRangeException(nameof(seed1), "At least one seed must be non-zero.");
+			_xState = seed1;
+			_yState = seed2;
+		}
+
+		/// <summary>
+		/// Implements <code>romuDuo_random</code> from <a href="http://romu-random.org/code.c">romu-random.org</a>.
+		/// </summary>
+		public ulong Next()
+		{
+			ulong xp = _xState;
+			_xState = 15241094284759029579ul * _yState;
+#if !NETSTANDARD1_0
+			_yState = BitOperations.RotateLeft(_yState,36) + BitOperations.RotateLeft(_yState, 15) - xp;
+#else
+			static ulong RotateLeft(ulong value, int offset) => (value << offset) | (value >>(64 - offset));
+			_yState = RotateLeft(_yState,36) + RotateLeft(_yState, 15) - xp;
+#endif
+			return xp;
+		}
+
+		ulong _xState;
+		ulong _yState;
+	}
+}
---
+++ tests/RomuRandom.Tests/RomuDuoTests.cs
@@ -0,0 +1,55 @@
+using Xunit;
+
+namespace RomuRandom.Tests
+{
+	public class RomuDuoTests
+	{
+		[Fact]
+		public void Seed1()
+		{
+			var romu = new RomuDuo(1);
+			Assert.Equal(0ul, romu.Next());
+			Assert.Equal(15241094284759029579ul, romu.Next());
+			Assert.Equal(10666103186410274816ul, romu.Next());
+			Assert.Equal(12956788498585303815ul, romu.Next());
+			Assert.Equal(8486441079326769463ul, romu.Next());
+			Assert.Equal(2373551033348051550ul, romu.Next());
+			Assert.Equal(10003256602856207724ul, romu.Next());
+			Assert.Equal(4669124565037773249ul, romu.Next());
+			Assert.Equal(10655693834798971811ul, romu.Next());
+			Assert.Equal(3487178542431509811ul, romu.Next());
+		}
+
+		[Fact]
+		public void SeedNegative1()
+		{
+			var romu = new RomuDuo(-1);
+			Assert.Equal(0ul, romu.Next());
+			Assert.Equal(3205649788950522037ul, romu.Next());
+			Assert.Equal(6411299577901044074ul, romu.Next());
+			Assert.Equal(2082275244517482851ul, romu.Next());
+			Assert.Equal(4591672078256323753ul, romu.Next());
+			Assert.Equal(7558422119120640637ul, romu.Next());
+			Assert.Equal(10754078505426094992ul, romu.Next());
+			Assert.Equal(6671804195509426422ul, romu.Next());
+			Assert.Equal(12430949857410599120ul, romu.Next());
+			Assert.Equal(13792996980988152038ul, romu.Next());
+		}
+
+		[Fact]
+		public void ThreeSeeds()
+		{
+			var romu = new RomuDuo(1, 2);
+			Assert.Equal(1ul, romu.Next());
+			Assert.Equal(12035444495808507542ul, romu.Next());
+			Assert.Equal(6091112088061520053ul, romu.Next());
+			Assert.Equal(15247473810760332814ul, romu.Next());
+			Assert.Equal(4016093660068235111ul, romu.Next());
+			Assert.Equal(4041301874668610437ul, romu.Next());
+			Assert.Equal(1544744609702868527ul, romu.Next());
+			Assert.Equal(9070204474037005789ul, romu.Next());
+			Assert.Equal(8832412314208222048ul, romu.Next());
+			Assert.Equal(4071963928619261017ul, romu.Next());
+		}
+	}
+}
